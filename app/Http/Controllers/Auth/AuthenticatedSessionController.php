d7d5987331a7e60a7663c7ba03fa05db58370953
Nur Ariffin Mohd Zin <nurariffinmohdzin@gmail.com>
2023-11-03 21:30:03 +0800

    first commit

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        // Get the role of the authenticated user
+         $role = auth()->user()->role;
+
+        // Redirect based on the role of the user
+        if ($role === 'admin') {
+        
+            return redirect('/admin/manage-registration');
+
+        } else {
+
+            return redirect('/user/user-dashboard');
+
+        }  
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
---
+++ app/Models/TrainingRegistration.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TrainingRegistration extends Model
+{
+    use HasFactory;
+
+    public function training(){
+
+        return $this->belongsTo('App\Models\Training');
+    }
+
+    public function user(){
+
+        return $this->belongsTo('App\Models\User');
+    }
+}
